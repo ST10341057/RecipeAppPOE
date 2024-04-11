cd49f547a3d0e21167f93da12cb8c83ac064f749
Gladwin Bapela <130281256+ST10341057@users.noreply.github.com>
2024-04-11 18:01:22 +0200

    Added Method: "EnterRecipeDetails" allows user to enter recipe details.

--- RecipeAppPOE/Recipe.cs
+++ RecipeAppPOE/Recipe.cs
@@ -8,5 +8,32 @@ namespace RecipeAppPOE
 {
     internal class Recipe
     {
+        private string[] ingredients;
+        private double[] quantities;
+        private string[] units;
+        private string[] steps;
+
+        public void EnterRecipeDetails()
+        {
+            Console.WriteLine("Enter the number of ingredients: ");
+            int numIngredients = int.Parse(Console.ReadLine());
+
+            ingredients = new string[numIngredients];
+            quantities = new double[numIngredients];
+            units = new string[numIngredients];
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"Enter name of ingredient {i + 1}: ");
+                ingredients[i] = Console.ReadLine();
+
+                Console.WriteLine($"Enter quantity of ingredient {i + 1}: ");
+                quantities[i] = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Enter unit of measurement for ingredient {i + 1}: ");
+                units[i] = Console.ReadLine();
+            }
+
+        } 
     }
 }
