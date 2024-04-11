2d904e99da7f8dbd8469c5489259234316dfacc6
Gladwin Bapela <130281256+ST10341057@users.noreply.github.com>
2024-04-11 17:54:45 +0200

    Added a main menu to Program class to Enter Recipe Details, Display Recipe, Scale Recipe, Reset Quantities, Clear All Dat or Exit

--- RecipeAppPOE/Program.cs
+++ RecipeAppPOE/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RecipeAppPOE;
+using System;
 
 namespace RecipeApp
 
@@ -7,7 +8,46 @@ namespace RecipeApp
     {
         static void Main(string[] args)
         {
-           
+            // Create a Recipe object and a boolean variable to control the loop
+            Recipe recipe = new Recipe();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("1. Enter Recipe Details");
+                Console.WriteLine("2. Display Recipe");
+                Console.WriteLine("3. Scale Recipe");
+                Console.WriteLine("4. Reset Quantities");
+                Console.WriteLine("5. Clear All Data");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("Choose an option: ");
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        recipe.EnterRecipeDetails();
+                        break;
+                    case "2":
+                        recipe.DisplayRecipe();
+                        break;
+                    case "3":
+                        recipe.ScaleRecipe();
+                        break;
+                    case "4":
+                        recipe.ResetQuantities();
+                        break;
+                    case "5":
+                        recipe.ClearAllData();
+                        break;
+                    case "6":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option! Please choose again.");
+                        break;
+                }
+            }
         }
     }
-}            
+}          
