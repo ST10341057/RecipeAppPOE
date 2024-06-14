d9c58585f02d418bbc1cfd62ad8c808d365bd294
Gladwin Bapela <130281256+ST10341057@users.noreply.github.com>
2024-06-14 19:44:59 +0200

    added a window for adding recipes

--- RecipeAppWPF/RecipeDetailWindow.xaml.cs
+++ RecipeAppWPF/RecipeDetailWindow.xaml.cs
@@ -1,27 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RecipeAppWPF;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RecipeAppWPF
 {
-    /// <summary>
-    /// Interaction logic for RecipeDetailWindow.xaml
-    /// </summary>
     public partial class RecipeDetailWindow : Window
     {
-        public RecipeDetailWindow()
+        private Recipe currentRecipe;
+
+        public RecipeDetailWindow(Recipe recipe)
         {
             InitializeComponent();
+            currentRecipe = recipe;
+            DisplayRecipeDetails();
+        }
+
+        // Display the details of the selected recipe
+        private void DisplayRecipeDetails()
+        {
+            RecipeNameTextBlock.Text = currentRecipe.RecipeName;
+            IngredientsStackPanel.Children.Clear();
+            foreach (var ingredient in currentRecipe.Ingredients)
+            {
+                TextBlock ingredientTextBlock = new TextBlock
+                {
+                    Text = $"- {ingredient.Quantity} {ingredient.UnitOfMeasurement} of {ingredient.Name} ({ingredient.Calories} calories) ({ingredient.FoodGroup})"
+                };
+                IngredientsStackPanel.Children.Add(ingredientTextBlock);
+            }
+
+            StepsStackPanel.Children.Clear();
+            foreach (var step in currentRecipe.Steps)
+            {
+                TextBlock stepTextBlock = new TextBlock
+                {
+                    Text = $"{step.StepNumber}. {step.Description}"
+                };
+                StepsStackPanel.Children.Add(stepTextBlock);
+            }
+
+            TotalCaloriesTextBlock.Text = currentRecipe.TotalCalories.ToString();
+        }
+        // Event handler for the Scale button click
+        private void ScaleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ScaleRecipeWindow scaleWindow = new ScaleRecipeWindow(currentRecipe);
+            if (scaleWindow.ShowDialog() == true)
+            {
+                double scaleFactor = scaleWindow.ScaleFactor;
+                currentRecipe.Scale(scaleFactor);
+                DisplayRecipeDetails();
+            }
+        }
+        // Event handler for the Reset button click
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentRecipe.ResetIngredientValues();
+            DisplayRecipeDetails();
+        }
+        // Event handler for the Delete button click
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            RecipeRepository.Recipes.Remove(currentRecipe);
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewRecipesWindow viewRecipesWindow = new ViewRecipesWindow();
+            viewRecipesWindow.Show();
+            this.Close();
         }
     }
 }
